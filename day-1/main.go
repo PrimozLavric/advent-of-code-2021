3666a121e49716b13eedf7d7de3296689bd28da6
Primoz Lavric <primoz.lavric@cosylab.com>
2021-12-06 21:23:51 +0100

    Solved day 1 exercise.

---
+++ day-1/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// An application contains application wide data such as Logger.
+type application struct {
+	log *log.Logger
+}
+
+// readDepthReport reads depth report data from the file located on the provided path. It expects file to have a single
+// unsigned depth entry per line.
+func (app *application) readDepthReport(path string) ([]uint, error) {
+	file, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Defer close the file.
+	defer func() {
+		err = file.Close()
+
+		if err != nil {
+			app.log.Printf("Failed to close file: %s\n", path)
+		}
+	}()
+
+	var depthReportData []uint
+
+	// Read depth data. Each line should contain one entry.
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		depth, err := strconv.Atoi(fileScanner.Text())
+
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("Bad input file format. Could not convert line '%s' to int.\n", fileScanner.Text()))
+		}
+
+		if depth < 0 {
+			return nil, errors.New(fmt.Sprintf("Bad input file. Read negative depth '%d'\n", depth))
+		}
+
+		depthReportData = append(depthReportData, uint(depth))
+	}
+
+	return depthReportData, nil
+}
+
+// countSequentialIncrements counts number of neighbour entries in a slice where sequence[i - 1] < sequence[i].
+func (app *application) countSequentialIncrements(sequence []uint) uint {
+	numIncrements := uint(0)
+
+	for i := 1; i < len(sequence); i++ {
+		if sequence[i-1] < sequence[i] {
+			numIncrements++
+		}
+	}
+
+	return numIncrements
+}
+
+// countSequentialWindowSumIncrements counts number if entries where sum of sequence[i - windowSize:i] < sequence[(i+1) - windowSize:i+1]
+func (app *application) countSequentialWindowSumIncrements(sequence []uint, windowSize uint) (uint, error) {
+	if windowSize == 0 {
+		return 0, errors.New(fmt.Sprintf("Invalid window size %d.", windowSize))
+	}
+
+	// Special case where window size is larger or equal to sequence length.
+	if windowSize >= uint(len(sequence)) {
+		return 0, nil
+	}
+
+	// Compute initial window sum.
+	currentSum := uint(0)
+
+	for i := uint(0); i < windowSize; i++ {
+		currentSum += sequence[i]
+	}
+
+	// Compute number of running window sum increments.
+	numIncrements := uint(0)
+
+	for i := windowSize; i < uint(len(sequence)); i++ {
+		lastSum := currentSum
+		currentSum -= sequence[i-windowSize]
+		currentSum += sequence[i]
+
+		if currentSum > lastSum {
+			numIncrements++
+		}
+	}
+
+	return numIncrements, nil
+}
+
+func main() {
+	var depthFilePath = flag.String("file", "default.txt", "Path to the depth report file.")
+	var windowSize = flag.Uint("window-size", 1, "Size of the sum window.")
+	flag.Parse()
+
+	app := application{log.Default()}
+
+	// Read depth report from file provided by the user.
+	depthData, err := app.readDepthReport(*depthFilePath)
+
+	if err != nil {
+		app.log.Fatalf("Failed to read depth report (%s)\n", err.Error())
+	}
+
+	// Use exercise part 1 solution when window size is 1.
+	if *windowSize == 1 {
+		depthIncrementCount := app.countSequentialIncrements(depthData)
+
+		fmt.Printf("There are %d depth increments.\n", depthIncrementCount)
+	} else {
+		depthIncrementCount, err := app.countSequentialWindowSumIncrements(depthData, *windowSize)
+
+		if err != nil {
+			app.log.Fatalf("Encountered an error while counting the window sum increments (%s).", err.Error())
+		}
+
+		fmt.Printf("With window size %d, there are %d depth sum increments.\n", *windowSize, depthIncrementCount)
+	}
+}
